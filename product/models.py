071445e8fac4691796b6cc93845e999f1b8b0aac
Saf <saf.alawa@gmail.com>
2023-11-05 20:35:35 +0800

    added docstring to models.py

--- product/models.py
+++ product/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 class Product(models.Model):
+    """
+    This product model is the model used for the app, it defines the content rules of the app.
 
+    Rules:
+        Certain rules that the product model applies is the max length of the title of a product
+        being 200 characters.
+
+        The price of the product having a max digit of 10 with 2 decimal paces/
+
+        Only being able to show positive integers for the quantity of a product
+    """
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
