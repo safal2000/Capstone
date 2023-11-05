d91fbf10e15b789e36dbb98135209908bc3d24e7
Saf <saf.alawa@gmail.com>
2023-11-05 20:29:22 +0800

    Added docstring to models.py

--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Product(models.Model):
+
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
