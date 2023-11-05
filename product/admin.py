29b22b788a605162fc619f95075ad8fc75e82f93
Saf <saf.alawa@gmail.com>
2023-11-05 19:06:26 +0800

    Added docstring to admin.py

--- product/admin.py
+++ product/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Product
 
 # Register your models here.
-@admin.register(Product)
+@admin.register(Product)   
 class ProductAdmin(admin.ModelAdmin):
+    """
+    This class is used to allow the admin to edit certain aspects of the product information such as the title, the price, and the quantity of a product.
+    """
     list_display = ('title', 'price', 'quantity')
