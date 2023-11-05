996a978f55631243a3ea516d245fba147e3a1674
Saf <saf.alawa@gmail.com>
2023-11-05 20:27:13 +0800

    Added docstring to admin.py

--- product/apps.py
+++ product/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class ProductConfig(AppConfig):
+    """
+    This class allows certain configurations for the primary app in the Django project, the product app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product'
